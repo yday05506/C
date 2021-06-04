06de9af7f47127f9e86b50a03d8f076572a6bdec
yday05506 <85325266+yday05506@users.noreply.github.com>
2021-06-04 11:18:13 +0900

    [문법공부] while문을 for문으로

---
+++ 07-03 반복문.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(void)
+{
+	for (int num = 0; num < 5; num++)
+	{
+		printf("예예예 예예예 예예예 예예에\n");
+	}
+	
+	/*
+	while (num < 5)
+	{
+		printf("예예예 예예예 예예예 예예에\n");
+		num++;
+	}
+	*/
+}
