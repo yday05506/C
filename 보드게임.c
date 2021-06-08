7e46dbcecf7991bb1cdec9fa8612552e512ba43f
양다연 <85325266+yday05506@users.noreply.github.com>
2021-06-08 10:21:41 +0900

    [보드게임] 무인도 주사위를 전용 주사위로 대체(변수 새로 할당)

--- 보드게임.c
+++ 보드게임.c
@@ -82,7 +82,7 @@ void main(void)
 			{
 				getchar();
 				printf("%d 턴입니다.\n", i);
-				dice = rand() % 6 + 1;
+				int exit_dice = rand() % 6 + 1;
 				printf("주사위가 %d이(가) 나왔습니다.\n", dice);
 				// 주사위를 던져서 3이 나오면 무인도를 탈출
 				if (dice == 3)
