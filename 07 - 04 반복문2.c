861fe9fc8a421e408bc0ed9a84055f6e34191131
양다연 <85325266+yday05506@users.noreply.github.com>
2021-06-08 09:02:58 +0900

    [문법공부] 반복문 다시 연습 1

--- 07 - 04 반복문2.c
+++ 07 - 04 반복문2.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 void main(void)
 {
-	printf("1\n");
-	printf("2\n");
-	printf("3\n");
-	printf("4\n");
-	printf("5\n");
-	printf("6\n");
-	printf("7\n");
-	printf("8\n");
-	printf("9\n");
-	printf("10\n");
+	for (int a = 1; a <= 10; ++a)
+	{
+		printf("%d\n", a);
+	}
 }
