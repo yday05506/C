aad99e911890194b70bdd2427ec24ec9a319e258
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-06-28 13:53:28 +0900

    함수의 반환형 변환

---
+++ 08 - 01 함수의 구성요소.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// 두 개의 숫자를 더해서 반환하는 함수
+double sum(int num1, int num2)
+{
+	return (double)(num1 + num2) + 0.14;
+}
+
+void main(void)
+{
+	double a = sum(1, 2);
+	printf("%lf", a);
+}
