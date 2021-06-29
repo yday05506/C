831e653ca6b535741ad5166194d1564ddaba5122
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-06-29 10:14:01 +0900

    재귀함수 완성

--- 문법연습/08 - 04 재귀함수.c
+++ 문법연습/08 - 04 재귀함수.c
@@ -4,11 +4,12 @@ int factorial(int num)
 {
 	if (num == 1)
 		return 1;
+	return num * factorial(num - 1);
 }
 
 void main(void)
 {
-	int val = 1;
+	int val = 2;
 	int a;
 	a = factorial(val);
 	printf("%d 팩토리얼은 %d \n", val, a);
