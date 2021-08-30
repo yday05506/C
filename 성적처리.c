aebf9804bcb04e740312b1916f77373a830a4407
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-08-31 08:51:31 +0900

    여러 과목 추가
    C언어, 자바, 컴퓨터구조

--- 성적처리.c
+++ 성적처리.c
@@ -2,18 +2,54 @@
 
 void main(void)
 {
-	int score;
+	int C_score;
 	printf("점수를 입력하시오 : ");
-	scanf("%d", &score);
+	scanf("%d", &C_score);
 
-	if (90 <= score <= 100)
+	if (90 <= C_score && C_score <= 100)
 		printf("A");
-	else if (80 <= score < 90)
+	else if (80 <= C_score && C_score < 90)
 		printf("B");
-	else if (70 <= score < 80)
+	else if (70 <= C_score && C_score < 80)
 		printf("C");
-	else if (60 <= score < 70)
+	else if (60 <= C_score && C_score < 70)
 		printf("D");
-	else (50 <= score < 70)
+	else
 		printf("E");
+
+	printf("\n");
+
+	int JAVA_score;
+	printf("점수를 입력하시오 : ");
+	scanf("%d", &JAVA_score);
+
+	if (90 <= JAVA_score && JAVA_score <= 100)
+		printf("A");
+	else if (80 <= JAVA_score && JAVA_score < 90)
+		printf("B");
+	else if (70 <= JAVA_score && JAVA_score < 80)
+		printf("C");
+	else if (60 <= JAVA_score && JAVA_score < 70)
+		printf("D");
+	else
+		printf("E");
+
+	printf("\n");
+
+	int CA_score;
+	printf("점수를 입력하시오 : ");
+	scanf("%d", &CA_score);
+
+	if (90 <= CA_score && CA_score <= 100)
+		printf("A");
+	else if (80 <= CA_score && CA_score < 90)
+		printf("B");
+	else if (70 <= CA_score && CA_score < 80)
+		printf("C");
+	else if (60 <= CA_score && CA_score < 70)
+		printf("D");
+	else
+		printf("E");
+
+	printf("\n");
 }
