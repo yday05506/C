22a1905ec752fe93b73a565cae0bd11a8adbd2a5
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-09-28 11:53:07 +0900

    지역변수 바꾸기(call by value)

---
+++ 11 - 05 지역변수 바꾸기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void changeA(int *num);
+
+void main(void)
+{
+	int A = 3;
+	printf("바꾸기 전 %d\n", A);
+	//함수의 argument로 A의 주소값을 넘김
+	changeA(&A);
+	printf("바꾸기 후 %d\n", A);
+}
+
+void changeA(int *num)
+{
+	*num = 5;
+}
