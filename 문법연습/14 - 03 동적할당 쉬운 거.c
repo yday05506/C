5d827aca3a894c5301c30fbe0167a3c7f63e57fc
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-11-23 10:18:49 +0900

    동적할당 쉬운 버전

--- 문법연습/14 - 03 동적할당 쉬운 거.c
+++ 문법연습/14 - 03 동적할당 쉬운 거.c
@@ -6,7 +6,16 @@
 
 void main(void)
 {
+	//정적할당(a, ptr1은 지역변수이므로 stack 영역에 할당)
 	int a = 3;
 	int* ptr1 = &a;
+	*ptr1 = 4;
 	printf("%d \n", *ptr1);
+
+	//동적할당(malloc으로 할당된 공간은 heap 영역에 할당)
+	//다만 지역변수 ptr2는 stack 영역에 할당) 
+	int* ptr2 = (int*)malloc(sizeof(int));
+	*ptr2 = 10;
+	printf("%d \n", *ptr2);
+	free(ptr2);
 }
